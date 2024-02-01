5db319b68696cca4716a13175907a3d6cda7b633
fsbym <37413822+fsbym@users.noreply.github.com>
2024-02-01 13:51:39 -0500

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
 import "./Header.scss";
+import Logo from "../../assets/logo.svg";
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="header__title">
-                Movie Explorer
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="header1__logowrapper">
+        <Link to="/" className="header1__logolink">
+          <img src={Logo} alt="Logo" className="header1__logo" />
+        </Link>
+      </div>
+      <div className="header__title">Movie Explorer</div>
+    </div>
+  );
 }
 
 export default Header;
